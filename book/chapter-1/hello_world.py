0cb60845a6ae13645e02f5a3c0f693d180e47d43
kellen-sun <66171946+kellen-sun@users.noreply.github.com>
2020-05-30 13:50:03 -0400

    Add files via upload

---
+++ book/chapter-1/hello_world.py
@@ -0,0 +1,8 @@
+#This is my first program
+#examples of strings:
+#'hello' and 'vgh4l$+='
+#use double quotes
+#"don't"
+#use 3 single quotes
+#'''He said, "I can't believe it."'''
+print('Hello World!')
