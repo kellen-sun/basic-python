ba80e5164d3ecbaf60ccd932a13b89b3ea2cab90
kellen-sun <66171946+kellen-sun@users.noreply.github.com>
2020-05-30 13:50:44 -0400

    Add files via upload

---
+++ book/chapter-2/integer.py
@@ -0,0 +1,13 @@
+print(69)
+score = 78
+print(score)
+print(69+420)
+print(69-420)
+print(69*420)
+print(4/2)
+print(420/69)
+print(5**3)
+print(420%69)
+print(420//69)
+#neat trick for you
+print(4761**0.5)
---
+++ book/chapter-2/variable.py
@@ -0,0 +1,5 @@
+language = 'python'
+print(language)
+print('language')
+l = language
+print(l)
